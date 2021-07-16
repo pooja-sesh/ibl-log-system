4a53af93fa2d0c30127b81fba631aa119b451397
Pooja Seshadri <Pooja.Seshadri@reece.com.au>
2021-07-16 16:09:00 +1000

    Set up basic routing with hookrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import {Header} from "./components/Header";
 import {EntryForm} from "./features/EntryForm/EntryForm";
 import React from "react";
 import { Home } from "./pages/Home"
+import {useRoutes} from "hookrouter";
+import {Log} from "./pages/Log";
 
-function App() {
-    return (
-        <Home />
-    )
+const routes = {
+    '/': () => <Home />,
+    // '/logs/': () => <Log />
+    '/logs/:id': ({id}) => <Log id={id}/>
+}
+
+// function App() {
+//     return (
+//         <Home />
+//     )
+// }
+
+const App = () => {
+    const routeResult = useRoutes(routes);
+
+    return routeResult;
 }
 
 export default App;
---
+++ src/pages/Log.jsx
@@ -0,0 +1,9 @@
+import {Header} from "../components/Header";
+import {EntryForm} from "../features/EntryForm/EntryForm";
+import React from "react";
+
+export function Log() {
+    return (
+        <Header>EDIT LOG</Header>
+    )
+}
